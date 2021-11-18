357a74bc7f080bf3e34c55a5e3140ef32af4034f
thehaigo <sirannha@gmail.com>
2021-11-19 01:27:51 +0900

    set relation

--- lib/live_logger/loggers/map.ex
+++ lib/live_logger/loggers/map.ex
@@ -5,15 +5,15 @@ defmodule LiveLogger.Loggers.Map do
   schema "maps" do
     field :description, :string
     field :name, :string
-    field :user_id, :id
 
+    belongs_to :user, LiveLogger.Accounts.User
     timestamps()
   end
 
   @doc false
   def changeset(map, attrs) do
     map
-    |> cast(attrs, [:name, :description])
-    |> validate_required([:name, :description])
+    |> cast(attrs, [:name, :description, :user_id])
+    |> validate_required([:name, :description, :user_id])
   end
 end
