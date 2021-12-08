115c86d061736b143e6fc6fa44a98201f8c63ec7
thehaigo <sirannha@gmail.com>
2021-12-08 18:56:40 +0900

    add fallback 401,422

--- lib/live_logger_web/controllers/fallback_controller.ex
+++ lib/live_logger_web/controllers/fallback_controller.ex
@@ -13,4 +13,18 @@ defmodule LiveLoggerWeb.FallbackController do
     |> put_view(LiveLoggerWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(LiveLoggerWeb.ErrorView)
+    |> render(:"401")
+  end
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(LiveLoggerWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
 end
